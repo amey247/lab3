3ed275266183b46926147ca1867b229a6c25ea14
amey247 <82931448+amey247@users.noreply.github.com>
2023-01-29 00:02:46 -0800

    updated Lab 1 for resubmission

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,27 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+	@Test
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3}, input1);
+    int[] input2 = {4, 8, 16};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{16, 8, 4}, input2);
 	}
-
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[]{}, ArrayExamples.reversed(input1));
+    int[] input2 = {5, 9};
+    assertArrayEquals(new int[]{9, 5}, ArrayExamples.reversed(input2));
   }
+
+  /*@Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {1.0, 1.0, 1.0, 1.0};
+    assertArrayEquals(new double[]{1.0}, ArrayExamples.averageWithoutLowest(input1));
+  }*/
 }
